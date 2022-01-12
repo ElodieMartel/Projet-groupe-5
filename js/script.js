12a371fa66ec723ed9ac3ffc57a5b721c971c946
Elodie <elodie@air-de-cesar.home>
2022-01-12 16:20:51 +0100

    Ajout du gitignore et des 2 sections en bas de l'index + le footer

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Fonction du back to top pour scroller en haut
+function topFunction() {
+    document.body.scrollTop = 0; //Safari
+    document.documentElement.scrollTop = 0; //Chrome, Firefox, IE et Opera
+  }
