5c8f6043d08cda425b2f07c63b9d074492a459cd
Elodie <elodie@air-de-cesar.home>
2022-01-13 21:14:14 +0100

    Ajout du menu burger responsive + footer responsive + correction du bug de la colonne blanche en mobile sur l'index

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@ function topFunction() {
     document.body.scrollTop = 0; //Safari
     document.documentElement.scrollTop = 0; //Chrome, Firefox, IE et Opera
   }
+
+  function toggleMenu() {
+    const liensMenu = document.getElementById("liensMenu");
+    if (liensMenu.style.display === "block") {
+      liensMenu.style.display = "none";
+    } else {
+      liensMenu.style.display = "block";
+    }
+  }
